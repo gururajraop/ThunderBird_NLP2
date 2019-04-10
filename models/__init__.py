c8f0b75ae595e58fcd7c3f12241e96dcb96b4732
GURURAJA P RAO <gururajraop@gmail.com>
2019-04-10 11:47:24 +0200

    Fix the issues in argument parsing

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,31 @@
 import importlib
+from models.Base_model import BaseModel
 
+
+def find_model_using_name(model_name):
+    """
+    Import the module "models/[model_name]_model.py".
+    """
+    model_filename = "models." + model_name + "_model"
+    modellib = importlib.import_module(model_filename)
+    model = None
+    target_model_name = model_name.replace('_', '') + 'model'
+    for name, cls in modellib.__dict__.items():
+        if name.lower() == target_model_name.lower() \
+           and issubclass(cls, BaseModel):
+            model = cls
+
+    if model is None:
+        print("Missing file ", model_name, "_model.py in the models/ directory")
+        exit(0)
+
+    return model
+
+
+def get_option_setter(model_name):
+    """Return the static method <modify_commandline_options> of the model class."""
+    model_class = find_model_using_name(model_name)
+    return model_class.modify_commandline_options
 
 def create_model(opt):
     """
--- options.py
+++ options.py
@@ -1,8 +1,6 @@
 """
 University of Amsterdam: MSc AI: NLP2 2019 Spring
-
 Project 1: Lexical Alignment
-
 Team 3: Gururaja P Rao, Manasa J Bhat
 
 Author: Gururaja P Rao
@@ -10,6 +8,8 @@ Author: Gururaja P Rao
 
 import argparse
 import os
+import models
+import data
 
 class Options:
     """This class defines options used during both training and test time.
@@ -24,9 +24,9 @@ class Options:
     def initialize(self, parser):
         """Define the common options that are used in both training and test."""
         # basic parameters
-        parser.add_argument('--dataroot', required=True, help='path to input (both training and testing)')
+        parser.add_argument('--dataroot', default='./datasets', help='path to input (both training and testing)')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='Base path to save or load the trained checkpoints')
-        parser.add_argument('--name', required=True, help='Sub path to saved checkpoints (both training and testing)')
+        parser.add_argument('--name', default='IBM1', help='Sub path to saved checkpoints (both training and testing)')
         parser.add_argument('--mode', type=str, default='test', help='Sub path to saved checkpoints (both training and testing)')
 
         # model parameters
@@ -40,7 +40,9 @@ class Options:
 
         # additional parameters
         parser.add_argument('--epoch', type=int, default=0, help='The starting epoch. If 0 the model will be intialized freshly, else the model will be loaded by the checkpoint based on the epoch')
+
         self.initialized = True
+        self.isTrain = False
         return parser
 
     def gather_options(self):
@@ -51,7 +53,19 @@ class Options:
             parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
             parser = self.initialize(parser)
 
-        return parser.parse_known_args()
+        # get the basic options
+        opt, _ = parser.parse_known_args()
+        self.isTrain = True if opt.mode == "train" else False
+
+        # modify model-related parser options
+        model_name = opt.model
+        model_option_setter = models.get_option_setter(model_name)
+        parser = model_option_setter(parser, self.isTrain)
+        opt, _ = parser.parse_known_args()  # parse again with new defaults
+
+        # save and return the parser
+        self.parser = parser
+        return parser.parse_args()
 
     def print_options(self, opt):
         """Print and save options
@@ -59,11 +73,18 @@ class Options:
         It will print both current options and default values(if different).
         It will save options into a text file / [checkpoints_dir] / opt.txt
         """
-        pass
+        message = ''
+        message += '----------------- Options ---------------\n'
+        for k, v in sorted(vars(opt).items()):
+            default = self.parser.get_default(k)
+            comment = '\t[default: %s]' % str(default)
+            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
+        message += '----------------- End -------------------'
+        print(message)
 
     def parse(self):
         """Parse our options, create checkpoints directory suffix, and set up gpu device."""
-        opt, _ = self.gather_options()
+        opt = self.gather_options()
 
         self.print_options(opt)
 
