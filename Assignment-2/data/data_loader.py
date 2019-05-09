1ac439d4ebc1af18e593636d2a3004df923956e8
GURURAJA P RAO <gururajraop@gmail.com>
2019-05-09 15:28:38 +0200

    Change the data loading code to use the tokens instead of vocab of words

--- Assignment-2/data/data_loader.py
+++ Assignment-2/data/data_loader.py
@@ -7,6 +7,27 @@ Team 3: Gururaja P Rao, Manasa J Bhat
 from nltk.tree import Tree
 from collections import defaultdict
 
+class Data:
+    """
+    The base data that holds the vocabulary and the tokenized data
+    """
+    def __init__(self):
+        """"Initialization of the vocabulary and the word token"""
+        self.vocab = []
+        self.word2token = {}
+
+    def add_word(self, word):
+        """"Add a new word to the vocabulary and tokenizer"""
+        if word not in vocab:
+            self.vocab.append(word)
+            self.word2token[word] = len(self.vocab) - 1
+
+        return self.word2token[word]
+
+    def __len__(self):
+        """Returns the length of the vocabulary"""
+        return len(self.vocab)
+
 class DataLoader():
     """
     The data loading class
@@ -17,29 +38,24 @@ class DataLoader():
         """
         Initialization fo the DataLoader
         """
+        self.vocabulary = Data()
+
         if opt.mode == 'train':
             self.mode = 'train'
             self.train_data_path = opt.dataroot+"/Training/02-21.10way.clean"
             self.val_data_path = opt.dataroot + "/Validation/22.auto.clean"
+
             print("Processing Training data")
-            self.train_data, self.train_vocab = self.get_data(self.train_data_path)
-            self.train_size = len(self.train_data)
-            self.train_vocab_size = len(self.train_vocab)
-            print("Training Vocabulary size : ", self.train_vocab_size)
+            self.train_data = self.get_data(self.train_data_path)
 
             print("Processing Validation data")
-            self.val_data, self.val_vocab = self.get_data(self.val_data_path)
-            self.val_size = len(self.val_data)
-            self.val_vocab_size = len(self.val_vocab)
-            print("Validation Vocabulary size : ", self.val_vocab_size)
+            self.val_data = self.get_data(self.val_data_path)
         else:
             self.mode = 'test'
             self.test_data_path = opt.dataroot+"/Testing/23.auto.clean"
+
             print("Processing Testing data")
-            self.test_data, self.test_vocab = self.get_data(self.test_data_path)
-            self.test_size = len(self.test_data)
-            self.test_vocab_size = len(self.test_vocab)
-            print("Testing Vocabulary size : ", self.test_vocab_size)
+            self.test_data = self.get_data(self.test_data_path)
 
     def get_data(self, data_path):
         """
@@ -52,18 +68,20 @@ class DataLoader():
             The parsed data in a list format
         """
         file = open(data_path, 'r', encoding='utf8')
-        vocabulary = defaultdict(lambda: 0)
-        data = []
         lines = file.readlines()
+
+        tokens = []
         for line in lines:
             sentence = self.pre_process_data(line)
             for word in sentence:
-                vocabulary[word] += 1
-            data.append(sentence)
+                self.vocabulary.add_word(word)
+                tokens.append(self.vocabulary.word2token[word])
+
+        data = torch.LongTensor(tokens)
 
         file.close()
 
-        return data, vocabulary
+        return data
 
     def pre_process_data(self, line):
         """
